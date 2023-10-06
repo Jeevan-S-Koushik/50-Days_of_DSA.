9b5476b65e2d12c3fc2206c530a229e834af2031
Jeevan S Koushik <78801996+Jeevan-S-Koushik@users.noreply.github.com>
2023-10-06 23:08:13 +0530

    Update palindrome_substring.cpp

---
+++ palindrome_substring.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int expand(string s, int i,int j){
+        int count = 0;
+        while(i>=0 && j<s.length() && s[i] == s[j]){
+            count ++;
+            i--;;
+            j++;
+        }
+        return count;
+    }
+
+    int countSubstrings(string s) {
+        int count = 0;
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            int odd = expand(s,i,i);
+            count = count + odd;
+            int even = expand(s,i,i+1);
+            count = count + even;
+        }   
+        return count;
+    }
+};
